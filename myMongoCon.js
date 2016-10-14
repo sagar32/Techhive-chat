1a9d3304db2cb5cf6c642a3651dbabd11446594c
Sagar-PC\Sagar <chopada.sagar32@gmail.com>
2016-10-14 16:58:10 +0530

    connection file change

--- myMongoCon.js
+++ myMongoCon.js
@@ -1,7 +1,7 @@
 var mongoClient = require('mongodb').MongoClient;
 var _db;
-var URL = 'mongodb://localhost:27017/facebookChatDB';
-//var URL = 'mongodb://techhive:techhive@ds145405.mlab.com:45405/techhive';
+//var URL = 'mongodb://localhost:27017/facebookChatDB';
+var URL = 'mongodb://techhive:techhive@ds145405.mlab.com:45405/techhive';
 module.exports = {
     connectToServer: function (callback) {
         mongoClient.connect(URL, function (err, db) {
